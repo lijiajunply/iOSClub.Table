55cf2a96e3697c4acb10e2a109911df777df7165
李嘉俊 <2383690354@qq.com>
2023-09-17 11:57:08 +0800

    init proj

---
+++ TableData/SignContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace TableData;
+
+public class SignContext : DbContext
+{
+    public SignContext (DbContextOptions<SignContext> options)
+        : base(options) { }
+
+    public DbSet<SignModel> Students { get; set; }
+}
---
+++ TableData/SignModel.cs
@@ -0,0 +1,12 @@
+﻿namespace TableData;
+
+public class SignModel
+{
+    public string UserName { get; set; } = "";
+    public string Id { get; set; } = "";
+    public string Academy { get; set; } = "信息与控制工程学院";
+    public string PoliticalLandscape { get; set; } = "群众";
+    public string Gender { get; set; } = "男";
+    public string ClassName { get; set; } = "";
+    public string PhoneNum { get; set; } = "";
+}
