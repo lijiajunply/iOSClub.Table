6c33743345da478216f92b794149c077d667676e
李嘉俊 <2383690354@qq.com>
2023-09-23 10:33:16 +0800

    再试一下

--- iOSClub.Table/Data/SignModel.cs
+++ iOSClub.Table/Data/SignModel.cs
@@ -2,11 +2,11 @@
 
 public class SignModel
 {
-    public string UserName { get; set; } = "";
-    public string Id { get; set; } = "";
-    public string Academy { get; set; } = "信息与控制工程学院";
-    public string PoliticalLandscape { get; set; } = "群众";
-    public string Gender { get; set; } = "男";
-    public string ClassName { get; set; } = "";
-    public string PhoneNum { get; set; } = "";
+    public string UserName { get; set; }
+    public string Id { get; set; }
+    public string Academy { get; set; }
+    public string PoliticalLandscape { get; set; }
+    public string Gender { get; set; }
+    public string ClassName { get; set; }
+    public string PhoneNum { get; set; }
 }
