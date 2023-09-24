4b5a7d12724b4c7806661e9936e9375136748244
李嘉俊 <2383690354@qq.com>
2023-09-24 14:36:34 +0800

    转成Migrate试一下

--- iOSClub.Table/Program.cs
+++ iOSClub.Table/Program.cs
@@ -12,6 +12,7 @@ builder.Services.AddServerSideBlazor();
 builder.Services.AddAntDesign();
 builder.Services.AddDbContextFactory<SignContext>(opt =>
     opt.UseSqlite(builder.Configuration.GetConnectionString("SchoolContextSQLite")));
+
 builder.Services.AddCors(options =>
 {
     options.AddDefaultPolicy(
@@ -38,8 +39,18 @@ using (var scope = app.Services.CreateScope())
 {
     var services = scope.ServiceProvider;
 
-    var context = services.GetRequiredService<SignContext>();
-    context.Database.EnsureCreated();
+    try
+    {
+        var context = services.GetRequiredService<SignContext>();
+        context.Database.MigrateAsync();
+        
+        context.Dispose();
+    }
+    catch (Exception ex)
+    {
+        var logger = services.GetRequiredService<ILogger<Program>>();
+        logger.LogError(ex, "An error occurred creating the DB");
+    }
 }
 
 app.UseHttpsRedirection();
