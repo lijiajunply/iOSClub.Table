aa85e990e23b59fcb135b2836dbe5d278f6b77e9
李嘉俊 <2383690354@qq.com>
2023-09-26 18:30:05 +0800

    先写成这样

--- iOSClub.Table/Data/EventModel.cs
+++ iOSClub.Table/Data/EventModel.cs
@@ -1,6 +1,12 @@
-﻿namespace iOSClub.Table.Data;
+﻿using Microsoft.AspNetCore.Components;
+
+namespace iOSClub.Table.Data;
 
 public class EventModel
 {
+    public RenderFragment Extra { get; set; }
+    public string Description { get; set; }
     
+    public string Url { get; set; }
+    public string Title { get; set; }
 }
