02792fe1061dfee0b0fc13cdf1e43f5168667970
李嘉俊 <2383690354@qq.com>
2023-09-27 23:42:35 +0800

    asdf

---
+++ iOSClub.Table/Data/NavModel.cs
@@ -0,0 +1,7 @@
+﻿namespace iOSClub.Table.Data;
+
+public class NavModel
+{
+    public string Name { get; set; }
+    public string Url { get; set; }
+}
--- iOSClub.Table/Data/SignModel.cs
+++ iOSClub.Table/Data/SignModel.cs
@@ -1,4 +1,6 @@
-﻿namespace iOSClub.Table.Data;
+﻿using System.Text;
+
+namespace iOSClub.Table.Data;
 
 public class SignModel
 {
@@ -9,4 +11,19 @@ public class SignModel
     public string Gender { get; set; }
     public string ClassName { get; set; }
     public string PhoneNum { get; set; }
+
+    public override string ToString()
+    {
+        return $"{UserName},{Id},{Gender},{Academy},{PoliticalLandscape},{ClassName},{PhoneNum}";
+    }
+
+    public static string GetCsv(List<SignModel> models)
+    {
+        StringBuilder builder = new StringBuilder("姓名,学号,性别,学院,政治面貌,专业班级,电话号码");
+        foreach (var model in models)
+        {
+            builder.Append("\n"+model);
+        }
+        return builder.ToString();
+    }
 }
