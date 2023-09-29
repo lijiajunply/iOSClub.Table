3e3c1a3d6afc49cf54808093f5dc76bf4bf876ec
李嘉俊 <2383690354@qq.com>
2023-09-29 12:07:16 +0800

    基本完成

--- iOSClub.Table/Data/NavModel.cs
+++ iOSClub.Table/Data/NavModel.cs
@@ -2,6 +2,6 @@
 
 public class NavModel
 {
-    public string Name { get; set; }
-    public string Url { get; set; }
+    public string Name { get; init; } = "";
+    public string Url { get; init; } = "";
 }
