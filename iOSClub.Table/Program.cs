bb426b285b83faa7cccd26b7e249af1cc3ebaa2f
李嘉俊 <2383690354@qq.com>
2023-10-27 16:10:19 +0800

    最后试一下

--- iOSClub.Table/Program.cs
+++ iOSClub.Table/Program.cs
@@ -81,7 +81,6 @@ using (var scope = app.Services.CreateScope())
     var services = scope.ServiceProvider;
     var context = services.GetRequiredService<SignContext>();
     context.Database.Migrate();
-    context.Database.EnsureCreated();
     if (!context.Staffs.Any())
     {
         var model = new PermissionsModel()
