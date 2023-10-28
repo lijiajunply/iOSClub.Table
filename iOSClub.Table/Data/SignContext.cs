530195cfa542ec017b815b1c41fbbf64932b59ac
李嘉俊 <2383690354@qq.com>
2023-10-28 19:41:47 +0800

    去掉Migrate

--- iOSClub.Table/Data/SignContext.cs
+++ iOSClub.Table/Data/SignContext.cs
@@ -9,4 +9,10 @@ public class SignContext : DbContext
 
     public DbSet<SignModel> Students { get; set; }
     public DbSet<PermissionsModel> Staffs { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<SignModel>().ToTable("Students");
+        modelBuilder.Entity<PermissionsModel>().ToTable("Staffs");
+    }
 }
