84a3e737afa8eb88493d21f1e89d19b342d53685
李嘉俊 <2383690354@qq.com>
2023-11-11 15:43:46 +0800

    再试一下

--- iOSClub.Api/Program.cs
+++ iOSClub.Api/Program.cs
@@ -1,11 +1,63 @@
-var builder = WebApplication.CreateBuilder(args);
+using System.Text.Encodings.Web;
+using System.Text.Unicode;
+using iOSClub.Share.Data;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.AspNetCore.Components.Server.ProtectedBrowserStorage;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Storage;
+using Microsoft.Extensions.WebEncoders;
 
+var builder = WebApplication.CreateBuilder(args);
+var configuration = builder.Configuration;
 // Add services to the container.
+
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddScoped<ProtectedSessionStorage>();
+builder.Services.AddScoped<AuthenticationStateProvider, Provider>();
+builder.Services.AddOptions();
+builder.Services.AddAuthorizationCore();
+builder.Services.AddAuthentication(options =>
+    {
+        options.DefaultChallengeScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+        options.DefaultSignInScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+        options.DefaultAuthenticateScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+    })
+    .AddCookie(options =>
+    {
+        options.SlidingExpiration = false;
+        options.LoginPath = "/Login";
+        options.LogoutPath = "/Login";
+        options.Cookie.Name = ".iOSClub.Website.Cookie";
+        options.Cookie.HttpOnly = true;
+        options.Cookie.SecurePolicy = CookieSecurePolicy.SameAsRequest;
+        options.Cookie.SameSite = SameSiteMode.Lax;
+    });
+
+if (builder.Environment.IsDevelopment())
+{
+    builder.Services.AddDbContextFactory<SignContext>(opt =>
+        opt.UseSqlite(configuration.GetConnectionString("SQLite")));
+}
+else if (builder.Environment.IsProduction())
+{
+    builder.Services.AddDbContextFactory<SignContext>(opt =>
+        opt.UseMySQL(configuration.GetConnectionString("MySQL")!));
+}
+
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(
+        policy => { policy.WithOrigins("https://qm.qq.com"); });
+});
+builder.Services.Configure<WebEncoderOptions>(options =>
+    options.TextEncoderSettings = new TextEncoderSettings(UnicodeRanges.All));
 
 var app = builder.Build();
 
@@ -16,10 +68,52 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+    var context = services.GetRequiredService<SignContext>();
+    try
+    {
+        context.Database.Migrate();
+        context.Database.EnsureCreated();
+    }
+    catch
+    {
+        var databaseCreator = (RelationalDatabaseCreator)context.Database.GetService<IDatabaseCreator>();
+        databaseCreator.CreateTables();
+        context.Database.Migrate();
+    }
+    finally
+    {
+        if (!context.Staffs.Any())
+        {
+            var model = new PermissionsModel
+            {
+                UserId = "1906020412",
+                Identity = "Founder",
+                Name = "韩晨超"
+            };
+            context.Staffs.Add(model);
+        }
+    }
+
+    context.SaveChanges();
+    context.Dispose();
+}
+
+
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
-
+app.UseCors();
 app.MapControllers();
 
 app.Run();
+
+public class Provider : AuthenticationStateProvider
+{
+    public override Task<AuthenticationState> GetAuthenticationStateAsync()
+    {
+        throw new NotImplementedException();
+    }
+}
