02dbccb62c52f075101124b338b01cd9f0c85075
李嘉俊 <2383690354@qq.com>
2024-02-03 00:43:10 +0800

    添加EventModel

---
+++ iOSClub.Share/Data/EventModel.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace iOSClub.Share.Data;
+
+public class EventModel
+{
+    public string Title { get; set; }
+    [Key]
+    [Column(TypeName = "varchar(256)")]
+    public string Link { get; set; }
+    public string Cover { get; set; }
+    public string Digest { get; set; }
+}
--- iOSClub.Share/Data/SignContext.cs
+++ iOSClub.Share/Data/SignContext.cs
@@ -1,12 +1,29 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
 
 namespace iOSClub.Share.Data;
 
-public class SignContext : DbContext
+public sealed class SignContext : DbContext
 {
     public SignContext(DbContextOptions<SignContext> options)
-        : base(options) { }
+        : base(options)
+    {
+        Students = Set<SignModel>();
+        Staffs = Set<PermissionsModel>();
+        Events = Set<EventModel>();
+    }
 
-    public DbSet<SignModel> Students { get; set; }
-    public DbSet<PermissionsModel> Staffs { get; set; }
+    public DbSet<SignModel> Students { get; init; }
+    public DbSet<PermissionsModel> Staffs { get; init; }
+    public DbSet<EventModel> Events { get; init; }
+}
+
+public class DesignTimeDbContextFactory : IDesignTimeDbContextFactory<SignContext>
+{
+    public SignContext CreateDbContext(string[] args)
+    {
+        var optionsBuilder = new DbContextOptionsBuilder<SignContext>();
+        optionsBuilder.UseSqlite("Data Source=Data.db");
+        return new SignContext(optionsBuilder.Options);
+    }
 }
