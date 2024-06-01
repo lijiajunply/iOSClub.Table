794f70d667da8ba2d66ed1581b3fbae536708219
李嘉俊 <2383690354@qq.com>
2024-06-01 19:54:44 +0800

    试验一下服务器时间

--- iOSClub.Table/Models/SignRecord.cs
+++ iOSClub.Table/Models/SignRecord.cs
@@ -4,6 +4,9 @@ namespace iOSClub.Table.Models;
 
 public class SignRecord
 {
+    public static readonly OrgSignRecord ios = new("mqqapi://card/show_pslcard?src_type=internal&version=1&uin=952954710&card_type=group&source=external",
+        "https://qm.qq.com/cgi-bin/qm/qr?authKey=MUNgIj%2B1gnkiI175qAQla6EcR44Fa0APCv%2FLo1a7YIlYgpeg76Q%2BGYMoedb8gGHU&k=HvhhArSc7tzuySOhXsnmZ6RgLcWkzXgu&noverify=0");
+    
     #region Table
 
     public static readonly string[] Academies =
@@ -138,3 +141,5 @@ public class SignRecord
 
     #endregion
 }
+
+public record OrgSignRecord(string qqApi, string https);
